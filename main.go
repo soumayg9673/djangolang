eb627b9d32bc5fd075ae9b01fa81428f01a2d6ba
Soumay Gupta <118595898+soumayg9673@users.noreply.github.com>
2024-07-23 12:15:15 +0530

    init http server from main file

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
-func init() {
+import (
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
+	serverCtrl "github.com/soumayg9673/djangolang/djangolang"
+)
+
+func init() {
+	log.Println("DjanGoLang")
+
+	serverCtrl.ReadEnv()
+	serverCtrl.ConnectDb()
 }
 
 func main() {
+	serverCtrl.BeServer.RTR = mux.NewRouter()
+	log.Println("Initializing HTTP server on :8080")
+	err := http.ListenAndServe(":8080", serverCtrl.BeServer.RTR)
+	if err != nil {
+		log.Fatalf("Error starting in server. Error %v", err)
+	}
 
+	serverCtrl.BeServer.DB.Close()
 }
